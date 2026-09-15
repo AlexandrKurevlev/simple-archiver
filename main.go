e7c59521b75842d9be7fb286441b8930e504dadb
Alexandr Kurevlev <alexandrkurevlev@gmail.com>
2026-09-15 23:04:15 +0300

    Add count repeating func

--- main.go
+++ main.go
@@ -22,6 +22,25 @@ func (sa *SimpleArchiver) compressEmpty(data []byte) []byte {
 	return data
 }
 
+func (sa *SimpleArchiver) countRepeating(data []byte) []byte {
+	if len(data) == 0 {
+		return []byte{}
+	}
+
+	res := make([]byte, 0)
+	var count byte
+	for i := 1; i < len(data); i++ {
+		if data[i] == data[i-1] {
+			count++
+		} else {
+			res = append(res, count, data[i-1])
+			count = 1
+		}
+	}
+	res = append(res, count, data[len(data)-1])
+	return res
+}
+
 func main() {
 	fmt.Println("Простой архиватор запущен")
 }
