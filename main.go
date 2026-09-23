64f6991bf1498a2727608ddb12f6113bf140f65d
Alexandr Kurevlev <alexandrkurevlev@gmail.com>
2026-09-23 22:26:33 +0300

    Add decompress func

--- main.go
+++ main.go
@@ -123,7 +123,7 @@ func (sa *SimpleArchiver) decompress(data []byte) []byte {
 	for i < len(data) {
 		l := data[i] & 127
 		isCompressed := data[i]&128 == 1
-		fmt.Println(data[i])
+		fmt.Printf("%x, %b\n", data[i], data[i])
 		if isCompressed {
 			i += 2
 		} else {
